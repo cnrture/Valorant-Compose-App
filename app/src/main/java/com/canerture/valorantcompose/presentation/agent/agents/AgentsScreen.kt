5d6fd12a3f0ca43ca20e4bb3ee9fa703efdf803d
Cengiz TORU <cengiztoru@gmail.com>
2022-08-30 23:56:50 +0300

    Implement searching agent functionality

--- app/src/main/java/com/canerture/valorantcompose/presentation/agent/agents/AgentsScreen.kt
+++ app/src/main/java/com/canerture/valorantcompose/presentation/agent/agents/AgentsScreen.kt
@@ -1,6 +1,7 @@
 package com.canerture.valorantcompose.presentation.agent.agents
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -11,9 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.canerture.valorantcompose.R
 import com.canerture.valorantcompose.common.components.ErrorText
+import com.canerture.valorantcompose.common.components.SearchBar
 
 @Composable
 fun AgentsScreen(
@@ -22,19 +26,32 @@ fun AgentsScreen(
 ) {
 
     val state = viewModel.state.value
+    val searchQuery = viewModel.searchQuery.value
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            contentPadding = PaddingValues(12.dp)
-        ) {
-            items(state.agents) { agentItem ->
-                AgentItem(
-                    agent = agentItem,
-                    onItemClick = {
-                        navigateToAgentDetail.invoke(it)
-                    }
-                )
+    Box {
+        Column(modifier = Modifier.fillMaxSize()) {
+            SearchBar(
+                searchText = searchQuery,
+                placeholderText = stringResource(R.string.search_agent),
+                onSearchTextChanged = {
+                    viewModel.searchAgent(it)
+                },
+                onClearClick = {
+                    viewModel.clearSearchQuery()
+                })
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                contentPadding = PaddingValues(12.dp)
+            ) {
+                items(state.agents) { agentItem ->
+                    AgentItem(
+                        agent = agentItem,
+                        onItemClick = {
+                            navigateToAgentDetail.invoke(it)
+                        }
+                    )
+                }
             }
         }
 
--- app/src/main/java/com/canerture/valorantcompose/presentation/agent/agents/AgentsViewModel.kt
+++ app/src/main/java/com/canerture/valorantcompose/presentation/agent/agents/AgentsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.canerture.valorantcompose.common.Resource
+import com.canerture.valorantcompose.domain.model.Agent
 import com.canerture.valorantcompose.domain.usecase.agents.GetAgentsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
@@ -16,8 +17,12 @@ class AgentsViewModel @Inject constructor(
     private val getAgentsUseCase: GetAgentsUseCase
 ) : ViewModel() {
 
+    private var allAgents = listOf<Agent>()
     private val _state = mutableStateOf(AgentsState())
     val state: State<AgentsState> = _state
+
+    private var _searchQuery = mutableStateOf("")
+    val searchQuery: State<String> = _searchQuery
 
     init {
         getAgents()
@@ -29,6 +34,7 @@ class AgentsViewModel @Inject constructor(
                 is Resource.Success -> {
                     result.data?.let {
                         _state.value = AgentsState(agents = it)
+                        allAgents = it
                     }
                 }
                 is Resource.Error -> {
@@ -40,5 +46,31 @@ class AgentsViewModel @Inject constructor(
                 }
             }
         }.launchIn(viewModelScope)
+    }
+
+    fun searchAgent(query: String) {
+        _searchQuery.value = query
+        _state.value = AgentsState(isLoading = true)
+
+        val foundAgents =
+            allAgents.filter {
+                it.displayName.contains(query, true) or
+                        it.description.contains(query, true)
+            }
+
+        if (foundAgents.isEmpty()) {
+            _state.value =
+                AgentsState(error = "No agents matching with your search")
+            return
+        }
+
+        _state.value = AgentsState(agents = foundAgents)
+
+    }
+
+    fun clearSearchQuery() {
+        _state.value = AgentsState(agents = allAgents)
+        _searchQuery.value = ""
+        return
     }
 }
